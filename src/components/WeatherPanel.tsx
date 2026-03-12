0170f56012035416fefbb638d153e3a2ab30d9b0
William Egan <egan.will@gmail.com>
2026-03-12 15:02:28 -0600

    fix: remove arrow from forecast link

--- src/components/WeatherPanel.tsx
+++ src/components/WeatherPanel.tsx
@@ -86,13 +86,10 @@ export default function WeatherPanel({ lat, lon }: { lat: number; lon: number })
 
       {/* Link to full forecast */}
       
-        href={weatherUrl}
+ href={weatherUrl}
         target="_blank"
         rel="noreferrer"
         style={{ display: "inline-block", fontSize: 12, color: "#aaa", border: "1px solid #2a2a2a", borderRadius: 8, padding: "6px 14px", textDecoration: "none" }}
       >
-        Full forecast &rarr;
+        Full forecast
       </a>
-    </div>
-  );
-}
